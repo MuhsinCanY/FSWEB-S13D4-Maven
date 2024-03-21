f37752bb6df778716b6cde8963a5621ee68496c2
MuhsinCanY <mcyilmaz1998@gmail.com>
2024-03-21 17:32:10 +0300

    enum updated

--- src/main/java/org/example/Weapon.java
+++ src/main/java/org/example/Weapon.java
@@ -1,11 +1,26 @@
 package org.example;
 
 public enum Weapon {
-    SWORD,
-    MEDIUM,
-    HIGH;
+    SWORD(10,5),
+    MEDIUM(10,5),
+    HIGH(10,5);
+
+    private int damage;
+    private double attactSpeed;
+
+    Weapon(int damage, double attackSpeed) {
+        this.damage = damage;
+        this.attactSpeed = attackSpeed;
+    }
 
     public int getDamage() {
+        return damage;
+    }
+    public double getAttackSpeed() {
+        return attactSpeed;
+    }
+
+    /*  public int getDamage() {
         return switch (this) {
             case SWORD -> 10;
             case MEDIUM -> 12;
@@ -19,5 +34,5 @@ public enum Weapon {
             case MEDIUM -> 1;
             case HIGH -> 3;
         };
-    }
+    }*/
 }
